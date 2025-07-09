81a50e86d21ca7769f346ef5856d14aedbef9b98
Jairo Lopes <73255336+JairoLopes@users.noreply.github.com>
2025-07-09 10:51:07 -0300

    up

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,6 @@
+import { slideUpFadeIn, growIn } from "../animation/animraions";
+import { motion } from "framer-motion";
+
 const Home = () => {
   return (
     <div className="relative w-full min-h-screen pt-navSpace flex flex-col items-center justify-center text-white p-6">
@@ -25,20 +28,34 @@ const Home = () => {
 
       {/* Conteúdo de Apresentação - Centralizado */}
       <div className="bg-black/40 -translate-y-10 relative z-10 text-center max-w-4xl mx-auto space-y-6 pb-3">
-        <h1 className="text-5xl md:text-7xl font-extrabold leading-tight tracking-wider drop-shadow-lg">
+        <motion.h1
+          initial={growIn.initial}
+          whileInView={growIn.animate}
+          transition={{ duration: 1, delay: 0.25 }}
+          className="text-5xl md:text-7xl font-extrabold leading-tight tracking-wider drop-shadow-lg"
+        >
           Astro<span className="text-secondaryTheme">lobby</span>
-        </h1>
-        <p className="text-xl md:text-2xl font-light max-w-2xl mx-auto drop-shadow-md">
+        </motion.h1>
+
+        <motion.p
+          initial={slideUpFadeIn.initial}
+          whileInView={slideUpFadeIn.animate}
+          transition={{ duration: 1, delay: 0.25 }}
+          className="text-xl md:text-2xl font-light max-w-2xl mx-auto drop-shadow-md"
+        >
           Sua porta de entrada para o cosmos. Desvende os mistérios do universo
           e explore as últimas descobertas.
-        </p>
+        </motion.p>
 
-        <a
+        <motion.a
+          initial={growIn.initial}
+          whileInView={growIn.animate}
+          transition={{ duration: 0.6, delay: 0 }}
           href="#notice"
           className="mt-8 active:scale-90 hover:bg-mainTheme inline-block bg-secondaryTheme active:sca text-white font-bold py-3 px-8 rounded-full transition duration-400 text-lg drop-shadow-lg"
         >
           Se atualize
-        </a>
+        </motion.a>
       </div>
     </div>
   );
